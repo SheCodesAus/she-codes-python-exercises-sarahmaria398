f22cef7dc104e2c7437ee0bb71c9b2859bfaf2ef
Sarah <sarahdavis398@gmail.com>
2022-04-30 09:47:44 +1000

    completion of the 2 class activities

--- variables_and_user_input/variables_input_exercises.py
+++ variables_and_user_input/variables_input_exercises.py
@@ -22,3 +22,16 @@
 # name = input("What is your name?")
 # height = input("How tall are you (cms)?")
 # print(f"{name} is {height}cms tall")
+
+#Program: 5
+# name = input("What is your name?")
+# age = input("What is your name?")
+# years_left = 90 - int(age)
+# print(f"You have {years_left} years to turn 90")
+
+#Program: 6
+
+# float1 = input("Please enter a number with a decimal")
+# float2 = input("Enter another number with a decimal")
+# result = float(float1)*float(float2)
+# print(int(result))
