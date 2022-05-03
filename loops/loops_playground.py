9f561123f872f6ddc412bff80ee783ca6da45783
Sarah <sarahdavis398@gmail.com>
2022-05-03 20:28:06 +1000

    Actioning of Loops Exercises in class

---
+++ loops/loops_playground.py
@@ -0,0 +1,55 @@
+# for i in range(0, 6):
+#     print(i)
+
+# range, inclusive of the beginning, exclusive of end.
+
+# for digit in range(8):
+#     print(digit)
+
+
+# for num in range(0, 11, 2):
+#     print(num)
+
+# like splice, 3rd argument is how many elements it will skip.
+
+
+# chilli_wishlist = ["igloo", "chicken", "donut toy", "cardboard box"]
+
+# for item in range(len(chilli_wishlist)):
+# print(item) # range(4), asking it to count the number of items in the list
+# print(chilli_wishlist[item])  # chilli_wishlist[2]
+
+# for item in chilli_wishlist:
+#     print(item) # same thing as above
+
+# chilli_wishlist = [
+#     ["igloo"],  # bed
+#     ["donut toy", "tennis ball", "crocodile toy"],  # toys
+#     ["chicken", "peanut butter"],  # treats
+#     ["cardboard box", "kong", "dig mat"]  # activity based toys
+# ]
+
+# for category in chilli_wishlist:
+# print(category) # each category list
+# for item in category:
+#     print(item)
+
+# WHILE LOOP
+
+# while 5 > 3:
+#     print("Hi") #never ends
+
+# num = 1
+# while num > 0:
+#     print("Hi")
+#     num = 0
+# stops code
+
+# guess = ""
+# while guess != "a":
+#     guess = input("Guess a letter: ")
+
+# counter = 10
+# while counter <= 10:
+#     print(counter)
+#     counter = counter + 1
---
+++ loops/loops_practice.py
@@ -0,0 +1,22 @@
+# Exercise : 1
+# num = int(input("Enter a number"))
+# for i in range(num+1):
+#     print(f"{num} * {i} = {num*i}")
+
+
+# my attempt
+# num = int(input("Enter a number"))
+# if int(num) == 0:
+#     print(num)
+
+# else:
+#     for i in range(0, 11):
+#         result = int(num) * i
+#         print(f"3{num} * {i} = {result}")
+
+
+# Exercise: 2
+num = int(input("Enter a number "))
+
+for i in range(num+1):
+    print(i+num)
