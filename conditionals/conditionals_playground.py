dff768afe04464ac84360c7993c7af566118b3b5
Sarah <sarahdavis398@gmail.com>
2022-10-03 18:48:18 +1000

    Update conditionals_playground.py

---
+++ conditionals/conditionals_playground.py
@@ -0,0 +1,58 @@
+
+# Program 1 & 2
+# moths_in_house = False
+# mitch_is_home = True
+
+# if moths_in_house and mitch_is_home:
+#     print("Hooman! Help me get the moths!")
+
+# if not moths_in_house and not mitch_is_home:
+#     print("No threats detected")
+
+# if moths_in_house and not mitch_is_home:
+#     print("Mewoooo! Hisss!")
+
+# if not moths_in_house and mitch_is_home:
+#     print("Climb on Mitch")
+
+
+# Program 3
+# light_colour = "Red"
+# car_detected = True
+
+# if light_colour == "Red" and car_detected:
+#     print("Flash!")
+# else:
+#     print("Do Nothing.")
+
+# Program 4
+# height = input("what is your height(cms)? ")
+# required_height = 120
+
+# if int(height) >= required_height:
+#     print("hop on")
+# else:
+#     print("sorry not today")
+
+
+# Program 5
+# username = "fleur"
+# password = "password123"
+
+# user_username = input("What is your username ")
+# user_password = input("Enter your password ")
+
+# if username == user_username and password == user_password:
+#     print("Correct!")
+# else:
+#     print("Incorrect!")
+
+# Program 6
+# at = "@"
+# dot = "."
+
+# email = input("What is your email? ")
+# if at and dot in email:
+#     print("valid email")
+# else:
+#     print("invalid email")
